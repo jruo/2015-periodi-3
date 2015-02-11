71d9f553b47cbc869563edaac6cb14fdf84e84f7
jruo <jruo@users.noreply.github.com>
2015-02-11 18:15:23 +0200

    lisää testikuvia

--- Tiralabra/src/test/java/com/hiilimonoksidi/tiralabra/MainTest.java
+++ Tiralabra/src/test/java/com/hiilimonoksidi/tiralabra/MainTest.java
@@ -10,6 +10,10 @@ public class MainTest {
         Main.main(tokenize("-i test-images/circles.png -o test-images/circles/ -s 0,0 -g 199,199"));
         Main.main(tokenize("-i test-images/lines.png -o test-images/lines/ -s 0,0 -g 299,299"));
         Main.main(tokenize("-i test-images/maze.png -o test-images/maze/ -s 2,0 -g 269,271"));
+        Main.main(tokenize("-i test-images/horizontal.png -o test-images/horizontal -s 250,20 -g 250,480"));
+        Main.main(tokenize("-i test-images/spray.png -o test-images/spray -s 0,0 -g 299,299"));
+        Main.main(tokenize("-i test-images/no_obstacles.png -o test-images/no_obstacles -s 0,50 -g 599,50"));
+        Main.main(tokenize("-i test-images/circles2.png -o test-images/circles2 -s 0,0 -g 199,199"));
         Main.main(tokenize("-i test-images/unsolvable.png -s 0,0 -g 1499,1499 -t 10"));
     }
     
